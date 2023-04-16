6dc048b0ee04a0737b69de46fcc1a23460281148
madhuri yadav <madhuri.y.0932@gmail.com>
2023-04-16 18:01:11 +0530

    jxzxz

--- server.js
+++ server.js
@@ -5,47 +5,60 @@ function register(event) {
     var password = document.getElementById("userPassword").value
     var confirmPassword = document.getElementById("userConfirmPassword").value
 
+    var data = {
+        namee: name,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword
+    }
+    var LS =JSON.parse(localStorage.getItem("USERS")) || []
+    LS.push(data);
+    localStorage.setItem("USERS", JSON.stringify(data));
+
     // console.log(name,"name here");
     // console.log(email,"email here");
     // console.log(password,"password here");
     // console.log(confirmPassword,"confirmPassword here");
 
-    if (name && email && password && confirmPassword) {
-        if (password.length >= 8 && confirmPassword.length >= 8) {
-            if (password == confirmPassword) {
-                // var data = object{}
-                var data = {
-                    nameee: name,
-                    email: email,
-                    password: password,
-                    confirmPassword: confirmPassword
-                }
-                // console.log("data is here",data);
-                // now store data in ls
-                localStorage.setItem("USERS", JSON.stringify(data))
-                // var fetchedData = JSON.parse(localStorage.getItem("USERS"));
-                // console.log(fetchedData);
-                //     console.log(JSON.parse(fetchedData));
-                // JSON.stringify(); convert object into json
-                // JSON.parse(); convert json into object
-                // localStorage.setItem(key,value) to save data in ls
-                // localStorage.getItem(key) to get data from ls
-                // localStorage.removeItem(key)to remove data from ls
+    // if (name && email && password && confirmPassword) {
+    //     if (password.length >= 8 && confirmPassword.length >= 8) {
+    //         if (password == confirmPassword) {
+    //             // var data = object{}
+                // var data = {
+                //     nameee: name,
+                //     email: email,
+                //     password: password,
+                //     confirmPassword: confirmPassword
+                // }
+    //             // console.log("data is here",data);
+    //             // now store data in ls
+    //             // // localStorage.setItem("USERS", JSON.stringify(data))
+                
+    //             //     console.log(JSON.parse(fetchedData));
+    //             // JSON.stringify(); convert object into json
+    //             // JSON.parse(); convert json into object
+    //             // localStorage.setItem(key,value) to save data in ls
+    //             // localStorage.getItem(key) to get data from ls
+    //             // localStorage.removeItem(key)to remove data from ls
+    //             var LS;
+    //             LS.push()
 
-            } else {
-                console.log("password not matched");
-            }
-        } else {
-            console.log("password should be 8 or more digit");
-        }
-    } else {
-        console.log("fill all the fields");
-    }
+    //         } else {
+    //             console.log("password not matched");
+    //         }
+    //     } else {
+    //         console.log("password should be 8 or more digit");
+    //     }
+    // } else {
+    //     console.log("fill all the fields");
+    // }
 
 }
 
 
-function getData() {
-   var fetchedData =JSON.parse(localStorage.getItem("USERS"));
-   console.log(fetchedData);
-}
+// function getData() {
+//    var fetchedData =JSON.parse(localStorage.getItem("USERS"));
+//    console.log(fetchedData);
+// }
+
+
